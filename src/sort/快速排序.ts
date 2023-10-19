cc20ff226cefa203bf4dd5b9c3560cd57b0e6f44
arky <847566733@qq.com>
2023-10-19 15:19:16 +0800

    更新快速排序算法

--- src/sort/快速排序.ts
+++ src/sort/快速排序.ts
@@ -4,27 +4,33 @@ const nums: number[] = new Array(50 * Math.random() >> 0).fill(0).map(() => Math
 /**
  * 快速排序（递归实现）
  * @param nums 
- * @param start 排序开始索引
- * @param end 排序结束索引
  */
-const quickSort = (nums: number[], start?: number, end?: number) => {
-    start ??= 0;
-    end ??= nums.length;
-    if (start >= end) return;
-    const num = nums[start];
-    let mid = start;
-    // 将小于等于 num 的值放在左边
-    for (let j = end - 1; j >= mid;) {
-        if (num > nums[j]) {
-            [nums[mid], nums[j]] = [nums[j], nums[mid]];
-            mid++;
+const quickSort = (nums: number[]) => {
+    /**
+     * 递归
+     * @param start 排序开始索引
+     * @param end 排序结束索引
+     */
+    const recursion = (start: number, end: number) => {
+        if (start >= end) return;
+        const key = nums[start];
+        let mid = start;
+        // 将小于等于 target 的值放在左边
+        for (let j = end - 1; j > mid; j--) {
+            if (key > nums[j]) {
+                // [nums[mid], nums[j]] = [nums[j], nums[mid]];
+                nums[mid] = nums[j];
+                // 始终保证 mid 指针指向的的值都大于 key
+                while (nums[mid] < key) mid++;
+                // 将当前 mid 指针指向的值放入之前 nums[j] 的位置
+                if (mid < j) nums[j] = nums[mid];
+            }
         }
-        else j--;
+        nums[mid] = key;
+        recursion(start, mid);
+        recursion(mid + 1, end);
     }
-    quickSort(nums, start, mid);
-    // 如果 mid === start 说明 nums[start] 右边的数都是大于 nums[start]
-    if (mid === start) mid++;
-    quickSort(nums, mid, end);
+    recursion(0, nums.length);
 }
 
 quickSort(nums);
@@ -37,5 +43,5 @@ console.log(nums);
  * 时间复杂度：O(nlogn)
  * 空间复杂度：O(1)
  * 扩展：分而治之的思想，所以在能并发的环境中，各个分块可以实现并发执行而互不干扰，从而进一步缩小时间开销
- * 代码第25行，可以知道，如果数组大部分甚至全部单调递增就很尴尬了：虽然不需要排序，但在黑盒中反而时间复杂度 O(n^2)。
+ * 代码第27-28行不难看出，如果数组大部分甚至全部单调递增就很尴尬了：虽然不需要排序，但在黑盒中反而时间复杂度 O(n^2)。
  */
